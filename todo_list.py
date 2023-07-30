1f63027bae6e1027fb0133bc791f3a9875e3f2ba
abshwabu <abdulazizredi90@gmail.com>
2023-07-30 23:09:54 +0300

    template code edited

--- todo_list.py
+++ todo_list.py
@@ -4,12 +4,12 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [  [sg.Text('task')],
+            [sg.Text('new task'), sg.InputText()],
+            [sg.Button('Add'), sg.Button('Cancel')] ]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Todo app', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
